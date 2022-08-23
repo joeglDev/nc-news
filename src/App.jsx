4b9cf134b3751b50b0657715cd07c83497dacc59
Hiroji <joegilbertdev@outlook.com>
2022-08-23 10:30:10 +0100

    updated styles on all articles

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Articles from './components/Articles.jsx';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Topics from './components/Topics.jsx';
 import ArticleByTopic from './components/ArticleByTopic.jsx';
+import SingleArticle from './components/SingleArticle';
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
     <Navbar></Navbar>
     <Routes>
     <Route path='/' element={<Articles/>}></Route>
+    <Route path='articles/:article_id' element={<SingleArticle/>}></Route>
     <Route path='/articles/topics' element={<Topics/>}></Route>
     <Route path='/articles/topics/:slug' element={<ArticleByTopic/>}></Route>
     </Routes>
---
+++ src/components/SingleArticle.jsx
@@ -0,0 +1,10 @@
+import {useState, useEffect} from 'react';
+import { useParams } from 'react-router-dom';
+
+const SingleArticle = () => {
+    const {article_id} = useParams();
+
+
+};
+
+export default SingleArticle;
